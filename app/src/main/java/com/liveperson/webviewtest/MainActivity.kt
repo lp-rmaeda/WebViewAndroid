a3ca839f78cf01d424ffa682ce35138f99afccd6
Ryosuke Maeda <rmaeda@liveperson.com>
2021-06-03 17:24:09 +0900

    Init

---
+++ app/src/main/java/com/liveperson/webviewtest/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.liveperson.webviewtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = this.findViewById(R.id.webview)
+        webView.loadUrl("https://ts-apac-rmaeda.s3.amazonaws.com/html/dflow_web.html")
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportZoom(false)
+        webView.settings.databaseEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.offscreenPreRaster = true
+    }
+}
